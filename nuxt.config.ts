a8d6e2aa37b8b611765de1642d445bb2099787b9
harodler <haj.rodler@gmail.com>
2025-04-01 22:13:47 +0200

    Squashed commit of the following:
    commit 1b06c9030b849f43176cab92fe8833b4d76bb3c4
    Merge: 57b0d0f 6a70be6
    Author: Hanna Rodler <63262915+hanna-rodler@users.noreply.github.com>
    Date:   Tue Apr 1 22:12:22 2025 +0200
    
        Merge pull request #3 from hanna-rodler/feature/seo
    
        chore: basic SEO implementation + favicon
    
    commit 6a70be62774b1d22be2801d0cd8efc3e20d3754a
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 22:11:50 2025 +0200
    
        chore: basic SEO implementation + favicon
    
    commit 57b0d0f24c5050342fc0fc3b56950e669514f5f4
    Merge: e72be9b b62e1ce
    Author: Hanna Rodler <63262915+hanna-rodler@users.noreply.github.com>
    Date:   Tue Apr 1 22:05:31 2025 +0200
    
        Merge pull request #1 from hanna-rodler/feature/curtain
    
        Feature/curtain
    
    commit b62e1cec671e40cfd1920478e3e2b2b95fa87931
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 22:03:35 2025 +0200
    
        fix: scrolling after curtain change
    
    commit f29f78fb97b90bf283e32b18aa5969bb6001e7bd
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 21:34:41 2025 +0200
    
        chore: listen for mobile resize
    
    commit 166ad64189bcf1bbca5afafff5d8f833d12da6f1
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 21:31:44 2025 +0200
    
        chore: update contact offset
    
    commit a1e4efd3474ac5aaf8cda4d3f71181b1d23f15b7
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 21:27:21 2025 +0200
    
        chore: fix navigation alignments
    
    commit 6ebfa4a224d3c5e97b60a118930eae94a922caa5
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 21:08:06 2025 +0200
    
        chore: improve curtain and handle curtain show via just root and /home
    
    commit f68c6282def0507911bc4063da3351aebf7d2e89
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 11:03:29 2025 +0200
    
        chore: improve curtain effect
    
    commit 747394e6543a08f56dc0b05f68bb45c44283f57e
    Author: harodler <haj.rodler@gmail.com>
    Date:   Tue Apr 1 01:22:57 2025 +0200
    
        chore: basic curtain, basic greetings creen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/fonts.css", "~/assets/css/main.css"],
+  ssr: true,
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -21,8 +22,55 @@ export default defineNuxtConfig({
       global: true,
     },
   ],
-  // nitro: {
-  //   preset: "node-server",
-  // },
-  modules: ["@nuxt/image", "@nuxt/icon", "@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Hann Julia Rodler",
+      htmlAttrs: {
+        lang: "de",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "android-chrome",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-chrome-192x192.png",
+        },
+        {
+          rel: "android-chrome",
+          type: "image/png",
+          sizes: "512x512",
+          href: "/android-chrome-512x512.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
+  site: {
+    url: "https://www.hannajuliarodler.com",
+    name: "Hanna Julia Rodler | Web Developer",
+  },
+  modules: [
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/sitemap",
+  ],
 });
